d3292d2500dc0619e83cec092e302f6d6a30ef5e
fritzhealy <fre.healy@gmail.com>
2018-05-28 09:14:06 -0400

    styling generic page and quicklinks sections

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,4 +10,15 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class("template-page"); ?>>
+	<div class="wrapper cap clear-bottom">
+		<section class="col-1">
+			<header>
+				<h1><?php the_title();?></h1>
+			</header>
+			<div class="copy">
+				<?php the_content();?>
+			</div><!--.copy-->
+		</section><!--.col-2-->
+		<?php get_template_part("template-parts/quicklinks");?>
+	</div><!--.wrapper-->
 </article><!-- #post-## -->
